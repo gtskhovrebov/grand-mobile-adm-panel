0c6dbe697f00eb1eb3f2ac5b95693148eea10cd0
gtskhovrebov <chovrebovgeorgij06@icloud.com>
2026-06-03 04:47:32 +0300

    Add files via upload
    Update to Grand Mobile Administration Panel v2.0

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='gm-admin-panel-ios-v2-final';
+const CACHE_NAME='gm-admin-panel-v2-final-liquid-contacts';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-152.png','./icon-167.png','./icon-180.png','./icon-192.png','./icon-512.png','./templates.json','./punishments.json','./senior_admin_templates.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS.filter(Boolean))))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
